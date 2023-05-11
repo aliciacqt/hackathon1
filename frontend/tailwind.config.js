0807c4318e3a2d376ba7cc5b70a9badf28d934d3
Luc <lefebureluc.42@gmail.com>
2023-05-11 16:23:52 +0200

    save for add the ilias-job

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ligthsand: "#F6E0CA",
+        sand: "#D8A477",
+        darksand: "#C77A4F",
+        blue: "#64A1B1",
+      },
+    },
   },
   plugins: [],
 };
