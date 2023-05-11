0f4f9740f11825425c7579cf419fa39df0582112
Oyunbi <camille.legloanic@laposte.net>
2023-05-11 16:28:19 +0200

    Ajout textes tableau ImageData

--- frontend/src/data/ImageData.js
+++ frontend/src/data/ImageData.js
@@ -6,38 +6,48 @@ const ImageData = [
     image2: "/src/assets/photos/japon2.jpg",
     image3: "/src/assets/photos/japon3.jpg",
     image4: "/src/assets/photos/japon4.jpg",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
   {
-    id: 1,
+    id: 2,
     destination: "Route 66",
     image1: "./assets/photos/route661",
     image2: "./assets/photos/route662",
     image3: "./assets/photos/route663",
     image4: "./assets/photos/route664",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
   {
-    id: 1,
+    id: 3,
     destination: "Edimbourgh",
     image1: "./assets/photos/edimbourg1",
     image2: "./assets/photos/edimbourg2",
     image3: "./assets/photos/edimbourg3",
     image4: "./assets/photos/edimbourg4",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
   {
-    id: 1,
+    id: 4,
     destination: "Svalbard",
     image1: "./assets/photos/svalbard1",
     image2: "./assets/photos/svalbard2",
     image3: "./assets/photos/svalbard3",
     image4: "./assets/photos/svalbard4",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
   {
-    id: 1,
+    id: 5,
     destination: "Golfe du Morbihan",
     image1: "./assets/photos/morbihan1",
     image2: "./assets/photos/morbihan2",
     image3: "./assets/photos/morbihan3",
     image4: "./assets/photos/morbihan4",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
 ];
 
