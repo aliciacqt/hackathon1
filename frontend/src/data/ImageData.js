26be8e36a8b969be52d9118da544a08b99ff85f8
Oyunbi <camille.legloanic@laposte.net>
2023-05-11 17:33:52 +0200

    Ajout photos au carousel

--- frontend/src/data/ImageData.js
+++ frontend/src/data/ImageData.js
@@ -6,6 +6,7 @@ const ImageData = [
     image2: "/src/assets/photos/japon2.jpg",
     image3: "/src/assets/photos/japon3.jpg",
     image4: "/src/assets/photos/japon4.jpg",
+    imageCar: "/src/assets/photos/japoncar.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
@@ -16,6 +17,7 @@ const ImageData = [
     image2: "./assets/photos/route662",
     image3: "./assets/photos/route663",
     image4: "./assets/photos/route664",
+    imageCar: "/src/assets/photos/route66car.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
@@ -26,6 +28,7 @@ const ImageData = [
     image2: "./assets/photos/edimbourg2",
     image3: "./assets/photos/edimbourg3",
     image4: "./assets/photos/edimbourg4",
+    imageCar: "/src/assets/photos/edimbourgcar.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
@@ -36,6 +39,7 @@ const ImageData = [
     image2: "./assets/photos/svalbard2",
     image3: "./assets/photos/svalbard3",
     image4: "./assets/photos/svalbard4",
+    imageCar: "/src/assets/photos/svalbardcar.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
   },
@@ -46,8 +50,24 @@ const ImageData = [
     image2: "./assets/photos/morbihan2",
     image3: "./assets/photos/morbihan3",
     image4: "./assets/photos/morbihan4",
+    imageCar: "/src/assets/photos/morbihancar.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
+  },
+  {
+    id: 6,
+    destination: "Hawaii",
+    image1: "./assets/photos/hawai1",
+    image2: "./assets/photos/hawai2",
+    image3: "./assets/photos/hawai3",
+    image4: "./assets/photos/hawai4",
+    imageCar: "/src/assets/photos/hawai3.jpg",
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus eros ut urna commodo, nec venenatis sapien venenatis. Quisque ut tincidunt arcu, porttitor consequat libero. Nunc lacinia felis nec enim egestas maximus. Aliquam erat volutpat. Phasellus sed cursus dui, in rhoncus leo. Donec nisl justo, aliquet a nisi in, ullamcorper varius leo. Suspendisse id neque laoreet tellus mattis venenatis id in ex. Phasellus magna massa, dapibus at mauris vitae, dictum fermentum sapien. Nullam in nibh luctus, luctus est vel, suscipit urna. Donec in felis eget est malesuada mattis at a erat. Sed pharetra eros elementum nulla auctor dignissim. ",
+  },
+  {
+    id: 7,
+    imageCar: "/src/assets/photos/hawai2.jpg",
   },
 ];
 
