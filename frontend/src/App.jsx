0596de6c2254d8ea40d9025a1f0261d976b05563
Oyunbi <camille.legloanic@laposte.net>
2023-05-11 17:40:37 +0200

    Changements validés

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import RoadTrip from "@pages/RoadTrip";
+import RoadTrip from "./pages/RoadTrip";
 import "./App.css";
 import Carousel from "./components/carousel";
 import Header from "./components/Header";
